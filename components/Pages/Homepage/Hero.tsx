6681420148db065ef5db3195d7fe2d52d8c52908
Lorenzo spinelli <iam.lorenzo.spinelli@gmail.com>
2022-11-27 21:51:37 +0100

    perf: icons + GA

--- components/Pages/Homepage/Hero.tsx
+++ components/Pages/Homepage/Hero.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 import logo from "../../../assets/images/firenzedev_darkbkg_shadow.svg";
 import H1 from "../../Heading/H1";
@@ -5,6 +6,9 @@ import H1 from "../../Heading/H1";
 export default function Hero() {
   return (
     <div className="grid lg:grid-cols-2 gap-2 bg-gradient-to-r from-blue-700 to-indigo-900">
+      <Head>
+        <link rel="preload" href={logo} as="image" />
+      </Head>
       <div className="text-white p-2 pt-6 lg:p-24 text-center lg:text-left">
         <H1 className="text-white">
           firenze<strong>.dev</strong> developer community
@@ -21,7 +25,13 @@ export default function Hero() {
         </div>
       </div>
       <div className="p-12 flex justify-center lg:justify-end lg:mr-24">
-        <Image src={logo} alt="firenze.dev logo" width={256} height={"256"} />
+        <Image
+          src={logo}
+          alt="firenze.dev logo"
+          width={256}
+          height={256}
+          priority
+        />
       </div>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["it"],
+    defaultLocale: "it",
+  },
+  siteUrl: process.env.SITE_URL || 'https://firenze.dev',
+  generateRobotsTxt: true,
 };
 
 module.exports = nextConfig;
