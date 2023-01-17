2eb81dd91648e69ac2312c7b447f35c4acde8d7c
Lorenzo spinelli <iam.lorenzo.spinelli@gmail.com>
2023-01-17 19:37:19 +0100

    feat: update contact page

--- components/Pages/Contacts/Contacts.tsx
+++ components/Pages/Contacts/Contacts.tsx
@@ -5,10 +5,19 @@ export default function Contacts() {
     <main className="container mx-auto p-2 pt-6 lg:px-56">
       <H1>Contatta firenze.dev</H1>
       <p className="py-8">
+        Puoi scriverci qualsiasi qualcosa, saremo molto felici di leggere e ti
+        risponderemo appena possiamo
+      </p>
+      <p className="py-2">
         Puoi contattare la community di firenze.dev all&apos;indirizzo{" "}
-        <a href="mailto:hello@firenze.dev">hello@firenze.dev</a> oppure
-        inviandoci un messaggio tramite questo form. O se preferisci tramite i
-        nosti canali social.
+        <a href="mailto:hello@firenze.dev">hello@firenze.dev</a>.
+      </p>
+      <p className="py-2">
+        Puoi scriverci anche sul{" "}
+        <a href="https://discord.com/invite/DJ5gpQtqMH">
+          discord della community
+        </a>
+        .
       </p>
     </main>
   );
