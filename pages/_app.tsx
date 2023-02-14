09dd392087121d638bfd8c2766a04d52f0781a12
Matteo Domenici <matteodome@gmail.com>
2023-02-14 22:39:03 +0100

    feat(GTM): add Google Tag Manager for manage tracking and analytics scripting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 import Layout from "../components/Layout/Layout";
 import PageTransitionLoader from "../components/Loader/PageTransitionLoader";
+import GTM from "../components/Analytics/GTM";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <PageTransitionLoader />
-      <Component {...pageProps} />
-    </Layout>
-  );
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <GTM/>
+            <Layout>
+                <PageTransitionLoader/>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
