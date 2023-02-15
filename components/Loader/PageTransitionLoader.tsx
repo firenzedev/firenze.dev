0eac229f4d5e9b1f220f9887e52a0be6a5a3984d
Matteo Domenici <matteodome@gmail.com>
2023-02-15 10:19:11 +0100

    fix: revert PageTransitionLoader.tsx

--- components/Loader/PageTransitionLoader.tsx
+++ components/Loader/PageTransitionLoader.tsx
@@ -2,8 +2,6 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import "nprogress/nprogress.css";
 import NProgress from "nprogress";
-import { pageview } from '../../lib/gtm'
-
 
 export default function PageTransitionLoader() {
   const router = useRouter();
@@ -19,14 +17,11 @@ export default function PageTransitionLoader() {
     router.events.on("routeChangeStart", handleStart);
     router.events.on("routeChangeComplete", handleComplete);
     router.events.on("routeChangeError", handleComplete);
-    router.events.on('routeChangeComplete', pageview);
 
     return () => {
       router.events.off("routeChangeStart", handleStart);
       router.events.off("routeChangeComplete", handleComplete);
       router.events.off("routeChangeError", handleComplete);
-      router.events.off('routeChangeComplete', pageview);
-
     };
   });
 
