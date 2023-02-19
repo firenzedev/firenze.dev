dbd57c96629ac61bc15100e5acdfe5d638f15c0d
Lorenzo spinelli <iam.lorenzo.spinelli@gmail.com>
2023-02-19 16:27:21 +0100

    feat: cookie consent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
 import PageTransitionLoader from "../components/Loader/PageTransitionLoader";
+import CookieConsent from "../components/CookieConsent/CookieConsent";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <PageTransitionLoader />
       <Component {...pageProps} />
+      <CookieConsent />
     </Layout>
   );
 }
