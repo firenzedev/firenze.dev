ad5fe98598c4e9ca7b6771a2c3f7b6c0d7521fc0
Matteo Domenici <matteodome@gmail.com>
2023-02-19 17:01:02 +0100

    build: remove preact step from build process

--- next.config.js
+++ next.config.js
@@ -12,10 +12,12 @@ const nextConfig = {
     if (!dev && !isServer) {
       config.resolve.alias = {
         ...config.resolve.alias,
+        /*
         "react/jsx-runtime.js": "preact/compat/jsx-runtime",
         react: "preact/compat",
         "react-dom/test-utils": "preact/test-utils",
         "react-dom": "preact/compat",
+        */
       };
     }
 
