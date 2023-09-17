16561342c0f0edcc284e4ab29de2ce073f379a64
Matteo Domenici <matteod@users.noreply.github.com>
2023-09-17 19:31:33 +0200

    feat: change newsletter form provider (from Brevo to Sender) (#31)

--- components/SubscribeForm/SubscribeForm.tsx
+++ components/SubscribeForm/SubscribeForm.tsx
@@ -1,17 +1,12 @@
 export default function SubscribeForm() {
   return (
-    <div>
-      <iframe
-        width="100%"
-        height="600"
-        src="https://c5cc198d.sibforms.com/serve/MUIEALSRiAoG-_6QOOQOUzTgELdNqbLzxToyhA3Ej-wrhpqb_dOvhXBk1EKbCP9aD4DZt6YcfjxLVc8nN8IXT5XVi0QADAjCYYCp8FQoonqQk3AofIzqDNEXJ816ysmsKcp6xt0PnD6rkoTduWkrs1SizkRRc8wcbjngNA8MazEXcp6HeHvmLD-8dT6bbi2wV1c1w04r-ZiROdw2"
-        frameBorder="0"
-        scrolling="auto"
-        allowFullScreen
-        loading="lazy"
-        className="w-full focus:outline-none"
-        title="Iscriviti a firenze.dev"
-      ></iframe>
+    <div
+        style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+        <div
+            style={{ textAlign: 'left' }}
+            className="sender-form-field"
+            data-sender-form-id="lmj7ikpkav4lnd7u1yq">
+        </div>
     </div>
   );
 }
