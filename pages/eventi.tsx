ff818f992fb528e87d03a9a8d75fd52e11d1fadf
Lorenzo Spinelli <iam.lorenzo.spinelli@gmail.com>
2023-10-30 21:49:42 +0100

    chore: event page meta description

--- pages/eventi.tsx
+++ pages/eventi.tsx
@@ -1,11 +1,17 @@
 import Head from "next/head";
 import Events from "../components/Pages/Events/Events";
 
+const description =
+  "Eventi per programmatori a Firenze, organizzati dalla community di svilupaptori firenze.dev. Scopri i nostri eventi per svilupaptori";
+
+
 export default function CommunityPage() {
   return (
     <>
       <Head>
         <title>Eventi - firenze.dev</title>
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
       </Head>
       <Events />
     </>
