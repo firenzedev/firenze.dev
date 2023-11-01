3c6fd779a9795cc542b7269bfa9810a1ebaa4191
Lorenzo Spinelli <iam.lorenzo.spinelli@gmail.com>
2023-11-01 14:52:48 +0100

    style: hr on footer

--- src/components/ui/Layout/Footer/Footer.tsx
+++ src/components/ui/Layout/Footer/Footer.tsx
@@ -65,7 +65,7 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <hr className="my-6 border-slate-300 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               Copyright &copy; {new Date().getFullYear()}{" "}
