27e0bbf00cd384206a358d133d959fc5bb350880
pierdomenico reitano <pierdomenico.reitano@farport.co>
2024-09-27 13:14:39 +0200

    Main (#39)
    * event: The unexpected attack: hacking the supply chain
    
    * chore: event page max width
    
    * feat: updated event
    
    * style: darkmode for header and footer
    
    ---------
    
    Co-authored-by: Lorenzo Spinelli <iam.lorenzo.spinelli@gmail.com>

--- src/components/ui/Layout/Header/Header.tsx
+++ src/components/ui/Layout/Header/Header.tsx
@@ -8,14 +8,14 @@ export default function Header() {
   return (
     <>
       <Menu navigation={navigation} />
-      <header className="fixed top-0 left-0 right-0 z-20 bg-white flex w-full shadow lg:min-h-[80px] p-2">
+      <header className="fixed top-0 left-0 right-0 z-20 bg-white dark:bg-gray-900 flex w-full shadow lg:min-h-[80px] p-2">
         <Logo />
         <div className="hidden sm:flex justify-evenly items-center flex-grow">
           {navigation.map((item) => (
             <Link
               href={item.href}
               key={item.name}
-              className="text-xl no-underline uppercase text-black inline-block px-3 py-2 border rounded border-white hover:underline"
+              className="text-xl no-underline uppercase text-black dark:text-white inline-block px-3 py-2 border rounded border-white hover:underline"
             >
               {item.name}
             </Link>
