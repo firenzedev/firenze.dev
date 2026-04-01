a6dd9e8b5c96587556a604436028f292064720f6
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2026-04-01 16:59:53 +0200

    feat: updated members description intro

--- src/app/about/_components/CoreTeam.tsx
+++ src/app/about/_components/CoreTeam.tsx
@@ -12,6 +12,9 @@ const coreTeam = [
     name: "Dario Pinzanuti",
     email: "dario@firenze.dev",
     linkedin: "https://www.linkedin.com/in/dario-pinzauti-developer/",
+    github: "https://github.com/Dario-Pinzauti",
+    intro:
+      "Sviluppatore backend (ex full stack, ma ognuno ha un passato di cui non va fiero). Da circa 8 anni passo le giornate a smanettare, rompere cose e fingere di sapere come sistemarle. Quando non scrivo codice, probabilmente sto giocando a qualcosa o cercando un nuovo film o una serie da vedere.",
     image: dario,
   },
   {
@@ -20,6 +23,8 @@ const coreTeam = [
     email: "riccardo@firenze.dev",
     linkedin: "https://www.linkedin.com/in/riccardo-figliozzi/",
     image: riccardo,
+    intro:
+      "Ho fatto il liceo scientifico con indirizzo informatico dove, in cinque anni, ho totalizzato la bellezza di un’ora di programmazione in Visual Basic. Dopo un primo approccio al marketing e la comunicazione, ho deciso di tuffarmi in Data Science e Machine Learning, principalmente per espiare i peccati di ignoranza del liceo. Ho sempre avuto la passione per i dati e l'automazione per due motivi onesti: mi permette di esercitare il mio talento naturale nel rompere le palle e asseconda la mia pigrizia cronica, visto che automatizzo qualunque cosa mi annoi. Oltre ad occuparmi di formazione in ambito AI e ML, nel tempo libero suono, leggo e coltivo il nobile hobby di sviluppare idee geniali che, ovviamente, non vedranno mai la luce in produzione.",
   },
   {
     anchor: "michele",
@@ -43,6 +48,8 @@ const coreTeam = [
     email: "mino@firenze.dev",
     linkedin: "https://www.linkedin.com/in/pierdomenico-reitano/",
     github: "https://github.com/mino89",
+    intro:
+      "Mi guadagno da vivere facendo lo sviluppatore frontend, se c'è bisogno non mi faccio problemi a mettere le mani anche sul backend. Ho mosso i miei primi passi sul web customizzando la pagina myspace per la mia band, ricordo con nostalgia quel delirio di <table> e <font>. Quando non sono davanti al computer ad imprecare contro il codice legacy, mi piace immergermi in campage di D&D, giocare a videogame e fare dei lunghi trekking in montagna.",
     image: mino,
   },
 ];
